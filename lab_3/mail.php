3a996a83ab586d978c02c9477331308426618531
Vladislavhgtech <51228868+Vladislavhgtech@users.noreply.github.com>
2022-10-19 18:50:32 +0300

    lab 3

---
+++ lab_3/mail.php
@@ -0,0 +1,15 @@
+<?php
+
+<!-- php обработчик  -->
+
+$recepient = "vld.85@inbox.ru";
+$siteName = "Ajax-форма";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$message = "Имя: $name \nТелефон: $phone";
+
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
